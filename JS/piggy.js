9bd22cfbe886c1d9770a30807444168c068db770
shaykat <shafinur.rahman.shaykat@gmail.com>
2022-02-17 23:46:08 +0600

    error message done

--- JS/piggy.js
+++ JS/piggy.js
@@ -1,9 +1,9 @@
-// Input field string to number convert
+// Input field string to number converting
 function ExpensesValueConvert(idName) {
     const input = document.getElementById(idName).value;
     const amount = parseFloat(input);
     //clear input field
-
+    document.getElementById(idName).value = '';
     return amount;
 }
 
@@ -12,25 +12,33 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const foodExpenses = ExpensesValueConvert('food-input');
     const rentExpenses = ExpensesValueConvert('rent-input');
     const clothExpenses = ExpensesValueConvert('cloth-input');
-
-    const totalAmount = foodExpenses + rentExpenses + clothExpenses;
-
     const incomeInput = document.getElementById('income-input').value;
 
+    if (incomeInput <= 0 || foodExpenses <= 0 || rentExpenses <= 0 || clothExpenses <= 0) {
+        alert('negative value does not allow');
+    }
+    else {
+        const totalAmount = foodExpenses + rentExpenses + clothExpenses;
+        const incomeInput = document.getElementById('income-input').value;
+        // get and update total expenses
 
-    // get and update total expenses
+        if (totalAmount <= incomeInput) {
+            const expensesTotal = document.getElementById('total-result');
+            const expenseTotalText = expensesTotal.innerText;
+            const previousExpensesTotal = parseFloat(expenseTotalText);
 
-    const expensesTotal = document.getElementById('total-result');
-    const expenseTotalText = expensesTotal.innerText;
-    const previousExpensesTotal = parseFloat(expenseTotalText);
+            const balanceTotal = document.getElementById('total-balance');
+            const balanceTotalText = parseFloat(balanceTotal.innerText);
+            const previousBalanceTotal = parseFloat(balanceTotalText);
 
-    const balanceTotal = document.getElementById('total-balance');
-    const balanceTotalText = parseFloat(balanceTotal.innerText);
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-
-    expensesTotal.innerText = previousExpensesTotal + totalAmount;
-    let NewBalance = incomeInput - totalAmount;
-    balanceTotal.innerText = previousBalanceTotal + NewBalance;
+            expensesTotal.innerText = previousExpensesTotal + totalAmount;
+            let NewBalance = incomeInput - totalAmount;
+            balanceTotal.innerText = previousBalanceTotal + NewBalance;
+        }
+        else {
+            alert('insufficient balance')
+        }
+    }
 })
 
 document.getElementById('savings-button').addEventListener('click', function () {
